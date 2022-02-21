fef35cfba56b7fd6ef2380c60c19340f3622e351
snsakib <hello@snsakib.com>
2022-02-21 07:58:18 +0600

    install bcrypt

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
 
+const posts = [
+  {
+    username: "Sakib",
+    title: "Post 1"
+  },
+  {
+    username: "Syed",
+    title: "Post 2"
+  }
+];
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
 app.listen(3000);
